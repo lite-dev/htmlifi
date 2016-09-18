ef29d7c6febe5eb699acae57f88e017f9185395e
lite-dev <jamzx1994@hotmail.com>
2016-09-18 12:02:54 -0400

    Update htmlifi_class.php
    Added additional comments.

--- htmlifi/htmlifi_class.php
+++ htmlifi/htmlifi_class.php
@@ -1,7 +1,7 @@
 <?php 
 	class HTMLIFI {
-		public static $PLUGIN_DIR;
-		public static $PLUGIN_URL;
+		public static $PLUGIN_DIR;     //Plugin Directory Path
+		public static $PLUGIN_URL;     //Plugin URL Path
 
 		public static $GLOBALS;        //GLOBAL OBJECT
 		public static $FUNCTIONS;      //FUNCTION OBJECT
@@ -11,26 +11,26 @@
 		public static $SETTINGS;       //SETTING OBJECT
 
 		function __construct() {
-			self::$PLUGIN_DIR = plugin_dir_path( __FILE__ );
-			self::$PLUGIN_URL = plugins_url() . '/htmlifi/';
+			self::$PLUGIN_DIR = plugin_dir_path( __FILE__ );                                //Initialize Directory Path
+			self::$PLUGIN_URL = plugins_url() . '/htmlifi/';                                //Initialize URL Path
 			
-			require_once( self::$PLUGIN_DIR . 'htmlifi_template.php' );
+			require_once( self::$PLUGIN_DIR . 'htmlifi_template.php' );                     //Require Template file
 
-			require_once( self::$PLUGIN_DIR . 'includes/htmlifi_globals_class.php' );
-			require_once( self::$PLUGIN_DIR . 'includes/htmlifi_functions_class.php' );
+			require_once( self::$PLUGIN_DIR . 'includes/htmlifi_globals_class.php' );       //Require GLOBAL CLASS
+			require_once( self::$PLUGIN_DIR . 'includes/htmlifi_functions_class.php' );     //Require FUNCTIONS CLASS
 
-			require_once( self::$PLUGIN_DIR . 'admin/htmlifi_admin_class.php' );
-			require_once( self::$PLUGIN_DIR . 'requests/htmlifi_requests_class.php' );
-			require_once( self::$PLUGIN_DIR . 'settings/htmlifi_settings_class.php' );
+			require_once( self::$PLUGIN_DIR . 'admin/htmlifi_admin_class.php' );            //Require ADMIN CLASS
+			require_once( self::$PLUGIN_DIR . 'requests/htmlifi_requests_class.php' );	//Require REQUESTS CLASS
+			require_once( self::$PLUGIN_DIR . 'settings/htmlifi_settings_class.php' );	//Require SETTINGS CLASS
 
-			require_once( self::$PLUGIN_DIR . 'uninstall.php' );
+			require_once( self::$PLUGIN_DIR . 'uninstall.php' );				//Require Uninstall file
 
-			self::$FUNCTIONS = new HTMLIFI_FUNCTIONS;
-			self::$GLOBALS = new HTMLIFI_GLOBALS;
+			self::$FUNCTIONS = new HTMLIFI_FUNCTIONS;	//Initialize static FUNCTIONS
+			self::$GLOBALS = new HTMLIFI_GLOBALS;		//Initialize static GLOBALS
 
-			self::$SETTINGS = new HTMLIFI_SETTINGS;
-			self::$ADMIN = new HTMLIFI_ADMIN;
-			self::$REQUESTS = new HTMLIFI_REQUESTS;
+			self::$SETTINGS = new HTMLIFI_SETTINGS;		//Initialize static SETTINGS
+			self::$ADMIN = new HTMLIFI_ADMIN;		//Initialize static ADMIN
+			self::$REQUESTS = new HTMLIFI_REQUESTS;		//Initialize static REQUESTS
 		}
 	}
 ?>
